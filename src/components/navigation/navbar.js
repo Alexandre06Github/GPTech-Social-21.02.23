be700120d3143a8b44e547497b6754c85a9e6622
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-02-21 13:39:41 +0100

    navbar

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -5,22 +5,39 @@ import Home from "../pages/home";
 import Register from "../pages/register";
 
 function Navbar() {
-	return (
-		<nav>
-			<div className="navbar">
-				<Link to="/register">Inscription</Link>
-				<div>
-					<Link to="/connection">Se connecter</Link>
-				</div>
-				<div>
-					<Link to="/">Accueil</Link>
-				</div>
-				<div>
-					<Link to="/home">Fil d'actualité</Link>
-				</div>
-			</div>
-		</nav>
-	);
+
+  return (
+	
+    <nav className="navbar">
+      <div>
+        <div className="bloc1">
+          <Link to="/register" className="bloc1">
+            Inscription
+          </Link>
+        </div>
+        <div>
+          <div className="bloc1">
+            <Link to="/connection" className="bloc1">
+              Se connecter
+            </Link>
+          </div>
+        </div>
+
+        <div className="classDiv"></div>
+
+        <div>
+          <div className="bloc1">
+            <Link to="/">Accueil</Link>
+          </div>
+        </div>
+        <div>
+          <div className="bloc1">
+            <Link to="/home">Fil d'actualité</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
