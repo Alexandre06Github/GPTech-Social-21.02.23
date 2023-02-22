299af07f75567844033f78c5513eab928c161770
JeremieG06300 <jeremie.garod@epitech.eu>
2023-02-22 14:37:28 +0100

    update perso.js

--- src/components/pages/perso.js
+++ src/components/pages/perso.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../navigation/logo.png";
 import Navbar from "../navigation/navbar";
 import Searchbar from "../navigation/searchbar";
 
