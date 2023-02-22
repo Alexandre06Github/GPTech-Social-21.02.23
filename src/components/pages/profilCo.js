14caf2feff57a5b2dfbe814d0b2471823ebef68d
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-02-22 11:44:36 +0100

    pushProfil

--- src/components/pages/profilCo.js
+++ src/components/pages/profilCo.js
@@ -6,36 +6,7 @@ import Searchbar from "../navigation/searchbar";
 import "./profilCo.css";
 
 function ProfilCo() {
-  const [firstName, setFistName] = useState([]);
-  const [lastName, setLastName] = useState([]);
-  const [email, setEmail] = useState([]);
-  const [age, setAge] = useState([]);
-  const [occupation, setOccupation] = useState([]);
 
-  async function handleSubmit() {
-    const options = {
-      method: "GET",
-      Headers: {
-        "Content-Type": "application/json",
-        Authorization: "bearer token",
-      },
-      body: JSON.stringify({
-        firstname: "test",
-        lastname: "test",
-        email: "test",
-        age: "test",
-        occupation: "test",
-      }),
-    };
-    console.log("option", options);
-
-    //Appel Api
-    await fetch(
-      `https://social-network-api.osc-fr1.scalingo.io/GPTech-social/user`,
-      options
-    )
-      .then((response) => response.json()) // Récupère la réponse au format JSON
-      .then((data) => console.log(data)); // Utilise les données renvoyées par l'API
 
     return (
       <div>
@@ -43,10 +14,11 @@ function ProfilCo() {
         <Navbar />
         <div className="container">
           <h1>Informations profil</h1>
+          <p>je fais un</p>
         </div>
         <Footer/>
       </div>
     );
   }
-}
+
 export default ProfilCo;
