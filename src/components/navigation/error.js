bd2aee6da04d502edb1efd39df17c729f6628253
Systolicious <123855559+Systolicious@users.noreply.github.com>
2023-02-23 20:07:04 +0100

    coschanges

--- src/components/navigation/error.js
+++ src/components/navigation/error.js
@@ -3,14 +3,24 @@ import Navbar from "../navigation/navbar";
 import Searchbar from "../navigation/searchbar";
 import Footer from "../navigation/footer";
 import "../../Styles/navStyle.css";
+import robots12 from "../../Images/robots12.jpg";
 
 function Error() {
 	return (
 		<div>
 			<Searchbar />
-			<Navbar />
-			<div className="container">
-				<h1>Game Over</h1>
+			<div className="homeBody">
+				<Navbar />
+				<div className="container">
+					<h1 className="pageTitle">Game Over</h1>
+				</div>
+				<div className="advert">
+					<img
+						src={robots12}
+						alt="advert"
+						className="bot"
+					/>
+				</div>
 			</div>
 			<Footer />
 		</div>
