ad7dc497483f8d451fbf56503fe821a413f43fd0
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-02-23 10:35:31 +0100

    pushinutil

--- src/components/pages/profilCo.js
+++ src/components/pages/profilCo.js
@@ -5,7 +5,6 @@ import Searchbar from "../navigation/searchbar";
 import "../../Styles/index.css";
 
 function ProfilCo() {
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -17,72 +16,63 @@ function ProfilCo() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "bearer " + localStorage.getItem("token")
-      }, 
+        Authorization: "bearer " + localStorage.getItem("token"),
+      },
     };
-    console.log("option", options);
-    
 
-	// Envoie une requête fetch avec l'URL de l'API et les options définies
-		const response = await fetch(
-			`https://social-network-api.osc-fr1.scalingo.io/gptech-social/user`,
-			options
-		);
-    
+    // Envoie une requête fetch avec l'URL de l'API et les options définies
+    const response = await fetch(
+      `https://social-network-api.osc-fr1.scalingo.io/gptech-social/user`,
+      options
+    );
 
-
-		// Récupère la réponse au format JSON
+    // Récupère la réponse au format JSON
     const data = await response.json();
-    setFirstName(data.firstName);
-    setLastName(data.lastName);
+    setFirstName(data.firstname);
+    setLastName(data.lastname);
     setEmail(data.email);
     setAge(data.age);
     setOccupation(data.occupation);
 
-		console.log(data); // Utilise les données renvoyées par l'API
+    console.log(data); // Utilise les données renvoyées par l'API
   }
 
-useEffect(() => {
- 
+  useEffect(() => {
+    getInfoProfil();
+  }, []);
 
-  getInfoProfil();
+
   
-}, [])
-
-
-
-
   return (
     <div>
       <Searchbar />
       <Navbar />
       <div className="container">
-       
         <h1>Informations profil</h1>
         <div className="Profil">
           <div action="" className="mx-auto" method="get">
-            <label htmlFor="">Nom:</label>
-            <div
+            <label htmlFor="">Nom :</label>
+            <input
               type="text"
               id="Nom"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
           <div action="" className="mx-auto" method="get">
             <div>
-              <label htmlFor="">Prénom:</label>
-              <div
+              <label htmlFor="">Prénom :</label>
+              <input
                 type="text"
                 id="Prenom"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
             <div action="" className="mx-auto" method="get">
               <div>
-                <label htmlFor="">email:</label>
-                <div
+                <label htmlFor="">Email :</label>
+                <input
                   type="text"
                   id="email"
                   value={email}
@@ -91,8 +81,8 @@ useEffect(() => {
               </div>
               <div action="" className="mx-auto" method="get">
                 <div>
-                  <label htmlFor="">age:</label>
-                  <div
+                  <label htmlFor="">Age :</label>
+                  <input
                     type="text"
                     id="age"
                     value={age}
@@ -101,15 +91,14 @@ useEffect(() => {
                 </div>
                 <div action="" className="mx-auto" method="get">
                   <div>
-                    <label htmlFor="">occupation:</label>
-                    <div
+                    <label htmlFor="">Occupation:</label>
+                    <input
                       type="text"
                       id="age"
                       value={occupation}
                       onChange={(e) => setOccupation(e.target.value)}
                     />
                   </div>
-                 
                 </div>
               </div>
             </div>
