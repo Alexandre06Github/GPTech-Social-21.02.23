00d9da2a078c012476d470f9b912a018a253491f
Systolicious <123855559+Systolicious@users.noreply.github.com>
2023-02-24 11:54:27 +0100

    commit

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -7,80 +7,91 @@ import robots5 from "../../Images/robots5.jpg";
 import Searchbar from "../navigation/searchbar";
 
 function Home() {
-  const [allPosts, setAllPosts] = useState([]);
+	const [allPosts, setAllPosts] = useState([]);
 
-  const renderMyPosts = () => {
-    if (allPosts.length >= 0) {
-      return allPosts.map((item, index) => {
-        return (
-          <div key={index}>
-            <p>{item.title}</p>
-            <p>{item.content}</p>
-          </div>
-        );
-      });
-    }
-  };
-  // permet de changer ma variable d'état grace à mon input
+	const renderMyPosts = () => {
+		if (allPosts.length >= 0) {
+			return allPosts.map((item, index) => {
+				return (
+					<div key={index}>
+						<p>{item.title}</p>
+						<p>{item.content}</p>
+					</div>
+				);
+			});
+		}
+	};
+	// permet de changer ma variable d'état grace à mon input
 
-  async function getAllPost() {
-    // Fonction pour récupérer les informations de l'utilisateur
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+	async function getAllPost() {
+		// Fonction pour récupérer les informations de l'utilisateur
+		const options = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    const response = await fetch(
-      // Envoie une requête fetch avec l'URL de l'API et les options définies
-      `https://social-network-api.osc-fr1.scalingo.io/gptech-social/posts?page=0&limit=10`,
-      options
-    );
-    const data = await response.json(); // Récupère la réponse au format JSON
-    setAllPosts(data.posts);
+		const response = await fetch(
+			// Envoie une requête fetch avec l'URL de l'API et les options définies
+			`https://social-network-api.osc-fr1.scalingo.io/gptech-social/posts?page=0&limit=10`,
+			options
+		);
+		const data = await response.json(); // Récupère la réponse au format JSON
+		setAllPosts(data.posts);
 
-    console.log(data.posts); // Utilise les données renvoyées par l'API
-  }
+		console.log(data.posts); // Utilise les données renvoyées par l'API
+	}
 
-  useEffect(() => {
-    getAllPost();
-  }, []);
+	useEffect(() => {
+		getAllPost();
+	}, []);
 
-  return (
-    <div className="App">
-      {/* section on the top */}
-      <Searchbar />
-      <div className="homeBody">
-        {/* section on the left */}
-        <Navbar />
+	return (
+		<div className="App">
+			{/* section on the top */}
+			<Searchbar />
+			<div className="homeBody">
+				{/* section on the left */}
+				<Navbar />
 
-        {/* MIDDLE POST  */}
-        <div className="container">
-          <h1 className="pageTitle">Fil d'actualités</h1>
-          <h3>"Miroir, mon bot miroir..."</h3>
-          <div>
-            <div className="field">
-              <div action="" method="get" className="field">
-               {renderMyPosts()}
-              </div>
-            </div>
-           
-          </div>
-        </div>
+				{/* MIDDLE POST  */}
+				<div className="container">
+					<h1 className="pageTitle">Fil d'actualités</h1>
+					<h3>"Miroir, mon bot miroir..."</h3>
+					<div>
+						<div className="form-control2">
+							<div
+								action=""
+								method="get"
+								className="field"
+							>
+								{renderMyPosts()}
+							</div>
+						</div>
+					</div>
+				</div>
 
-        {/* advert on the right */}
-        <div className="advert">
-          <img src={robots1} alt="advert" className="bot"></img>
-          <img src={robots5} alt="advert" className="bot"></img>
-        </div>
-      </div>
-      {/* end of page */}
-      <div>
-        <Footer />
-      </div>
-    </div>
-  );
+				{/* advert on the right */}
+				<div className="advert">
+					<img
+						src={robots1}
+						alt="advert"
+						className="bot"
+					></img>
+					<img
+						src={robots5}
+						alt="advert"
+						className="bot"
+					></img>
+				</div>
+			</div>
+			{/* end of page */}
+			<div>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default Home;
