cd3aaff51e47bcc92f0888d6b55639d992d18627
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-02-24 14:16:43 +0100

    styleCss

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -47,10 +47,12 @@ function Home() {
       return allPosts.map((item, index) => {
         return (
           <div key={index}>
-            <p>{item.title}</p>
-            <p>{item.content}</p>
-            <button onClick={ () => like (item._id) }>like</button>    {/* quand tu clique sur le bouton, ajouter 1 */}
+            <div className="homeSpace">
+            <p className="contenuBloc">{item.title}</p>
+            <p className="contenuBloc">{item.content}</p>
+            <button className="buttonLike" onClick={ () => like (item._id) }>❤️</button>    {/* quand tu clique sur le bouton, ajouter 1 */}
             <span>{item.likes.length}</span>
+            </div>
           </div>
         );
       });
